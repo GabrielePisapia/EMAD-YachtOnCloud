26c464a27c3df5c1a34b1d31ed3a99749f9f20ae
GabrielePisapia <50744224+GabrielePisapia@users.noreply.github.com>
2022-01-11 18:43:19 +0100

    Update google_sign_in.dart
    Aggiunto inserimento sul db con google sign in

--- yachtoncloud/lib/google_sign_in.dart
+++ yachtoncloud/lib/google_sign_in.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:yachtoncloud/userSetup.dart';
 
 class GoogleSignInProvider extends ChangeNotifier {
   final googleSignIn = GoogleSignIn();
@@ -12,13 +13,21 @@ class GoogleSignInProvider extends ChangeNotifier {
     if (googleUser == null) return;
     _user = googleUser;
 
+    print(_user?.email);
+    final names = _user?.displayName!.split(' ');
+    final nome = names![0];
+    final cognome = names[1];
     final googleAuth = await googleUser.authentication;
 
     final credential = GoogleAuthProvider.credential(
       accessToken: googleAuth.accessToken,
       idToken: googleAuth.idToken,
     );
+
+    var firebaseUser = FirebaseAuth.instance.currentUser;
+    UserSetup(uid: firebaseUser!.uid).updateUserData(nome, cognome);
     await FirebaseAuth.instance.signInWithCredential(credential);
+
     notifyListeners();
   }
 }
