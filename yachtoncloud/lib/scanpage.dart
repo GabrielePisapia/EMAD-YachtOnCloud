484bc26e63bb0cee69f94c5f137333ba52b5322d
Simona Pentangelo <56032063+SimonaPentangelo@users.noreply.github.com>
2022-01-19 13:50:14 +0100

    Update scanpage (mancano cose nel pubspec)

---
+++ yachtoncloud/lib/scanpage.dart
@@ -0,0 +1,129 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Scan QR Code - Flutter Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ScanQRPage(),
+    );
+  }
+}
+
+class ScanQRPage extends StatefulWidget {
+  const ScanQRPage({Key? key}) : super(key: key);
+
+  @override
+  _ScanQRPageState createState() => _ScanQRPageState();
+}
+
+class _ScanQRPageState extends State<ScanQRPage> {
+  final GlobalKey qrKey = GlobalKey();
+  late QRViewController controller;
+  Barcode? result;
+//in order to get hot reload to work.
+  @override
+  void reassemble() {
+    super.reassemble();
+    if (Platform.isAndroid) {
+      controller.pauseCamera();
+    } else if (Platform.isIOS) {
+      controller.resumeCamera();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //appBar: AppBar(
+        //title: Text('Scan QR Code - Flutter Example'),
+      //),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 4,
+            child: Stack(
+              children: [
+                QRView(
+                  key: qrKey,
+                  onQRViewCreated: onQRViewCreated,
+                  overlay: QrScannerOverlayShape(
+//customizing scan area
+                    borderWidth: 10,
+                    borderColor: Colors.teal,
+                    borderLength: 20,
+                    borderRadius: 10,
+                    cutOutSize: MediaQuery.of(context).size.width * 0.8,
+                  ),
+                ),
+                Positioned(
+                  left: 0.0,
+                  right: 0.0,
+                  bottom: 0.0,
+                  child: Container(
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: Colors.black26,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        IconButton(
+                            icon: Icon(
+                              Icons.flip_camera_ios,
+                              color: Colors.white,
+                            ),
+                            onPressed: () async {
+                              await controller.flipCamera();
+                            }),
+                        IconButton(
+                            icon: Icon(
+                              Icons.flash_on,
+                              color: Colors.white,
+                            ),
+                            onPressed: () async {
+                              await controller.toggleFlash();
+                            })
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void onQRViewCreated(QRViewController p1) 
+{
+//called when View gets created. 
+    this.controller = p1;
+    controller.scannedDataStream.listen((scanData) async {
+       controller.pauseCamera();
+      setState(() {
+        result = scanData;
+      });
+      debugPrint("WOW THATS HOT");
+      controller.resumeCamera();
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+}
